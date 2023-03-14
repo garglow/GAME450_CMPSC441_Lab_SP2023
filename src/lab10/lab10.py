fb8d3b1b3e6d52bbc5b9b1fa58b36cd0c49ade2f
garglow <gmg5511@psu.edu>
2023-03-14 11:32:30 -0400

    lab10 normalize

--- src/lab10/lab10.py
+++ src/lab10/lab10.py
@@ -7,10 +7,11 @@ You can usually improve the model by normalizing the input data. Try that and se
 from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn import preprocessing
 import pandas as pd
 import numpy as np
 
-data = pd.read_csv("src/lab8/heart.csv")
+data = pd.read_csv("src/lab10/heart.csv")
 
 # Transform the categorical variables into dummy variables.
 print(data.head())
@@ -25,13 +26,20 @@ x_train, x_test, y_train, y_test = train_test_split(
 )
 
 """ Train a sklearn model here. """
-
-sklearn_model = None
+sklearn_model = KNeighborsClassifier(n_neighbors=11)
+sklearn_model.fit(x_train,y_train)
 
 # Accuracy
 print("Accuracy of model: {}\n".format(sklearn_model.score(x_test, y_test)))
+
+min_max_scaler = preprocessing.MinMaxScaler()
+x_train = min_max_scaler.fit_transform(x_train)
+x_test = min_max_scaler.fit_transform(x_test)
+sklearn_model.fit(x_train,y_train)
 
 
 """ Improve the model by normalizing the input data. """
 
 print("Accuracy of improved model: {}\n".format(sklearn_model.score(x_test, y_test)))
+
+
