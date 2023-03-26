d931aae682cbae089094f9722f4a7f2aa7e1711d
garglow <gmg5511@psu.edu>
2023-03-26 18:14:57 -0400

    Autononmous random movements and weapon selection

--- src/lab11/pygame_ai_player.py
+++ src/lab11/pygame_ai_player.py
@@ -1,12 +1,25 @@
 """ Create PyGameAIPlayer class here"""
+import pygame
+import random
+from lab11.turn_combat import CombatPlayer
 
 
 class PyGameAIPlayer:
-    pass
+    def __init__(self) -> None:
+        pass
+
+    def selectAction(self, state):
+        return ord(str(random.randint(1,9)))
 
 
 """ Create PyGameAICombatPlayer class here"""
 
 
-class PyGameAICombatPlayer:
-    pass
+class PyGameAICombatPlayer(CombatPlayer):
+    def __init__(self, name):
+        super().__init__(name)
+
+    def weapon_selecting_strategy(self):
+        while True:
+            self.weapon = random.randint(0,2)
+            return self.weapon
